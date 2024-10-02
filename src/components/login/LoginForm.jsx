61d142a26e14bd1a863cc44743f40a4b63f128e9
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-10-02 17:37:18 +0200

    🐛 F02: components > login > LoginForm > bad move

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,44 +1,25 @@
-import { useState } from 'react';
-import { signUp } from '../../API/user.js';
-import { useNavigate } from 'react-router-dom';
-import LoginForm from './LoginForm.jsx';
+import Button from "../../ui/Button.jsx";
+import Input from "../../ui/Input.jsx";
 
-export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const navigate = useNavigate();
-
-  const handleAuth = async (event) => {
-    event.preventDefault();
-    try {
-      let userCredential
-      userCredential = await signUp(email, password);
-      alert(`Bonjour ${userCredential.user.email}`);
-      console.log(userCredential);
-      setEmail('');
-      setPassword('');
-      navigate('/home');
-
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export default function LoginForm({ submit, button, email, password, changeEmail, changepassword }) {
 
   return (
-    <main>
-      <h1>Bienvenue chez nous !</h1>
-
-      <h2>Inscrivez-vous</h2>
-
-      <LoginForm
-        submit={(e) => handleAuth(e)}
-        button="S'inscrire"
-        email={email}
-        password={password}
-        changeEmail={(e) => setEmail(e.target.value)}
-        changepassword={(e) => setPassword(e.target.value)}
+    <form onSubmit={submit}>
+      <Input
+        type="email"
+        placeholder="entrez votre email"
+        value={email}
+        onChange={changeEmail}
+        required
       />
-    </main>
+      <Input
+        type="password"
+        placeholder="entrez votre mot de passe"
+        value={password}
+        onChange={changepassword}
+        required
+      />
+      <Button type={"submit"} text={button}/>
+    </form>
   );
 }
